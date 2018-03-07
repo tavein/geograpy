501b8b1124fa3ced428eea0aef41faaa6dc8cb84
shubhamkumar0 <shubham.kumar099@gmail.com>
2018-03-07 14:35:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='geograpy',
       install_requires=[
             'numpy',
             'nltk',
-            'newspaper3k'
+            'newspaper3k',
             'jellyfish',
             'pycountry'
       ],
