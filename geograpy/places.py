eb13a7cafa4ad01fe57c40bd68ff8aa14bec6f34
shubhamkumar0 <shubham.kumar099@gmail.com>
2018-03-10 12:49:50 +0530

    Update places.py

--- geograpy/places.py
+++ geograpy/places.py
@@ -13,7 +13,7 @@ class PlaceContext(object):
     def __init__(self, place_names, db_file=None):
         db_file = db_file or os.path.dirname(os.path.realpath(__file__)) + "/locs.db"
         self.conn = sqlite3.connect(db_file)
-        self.conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+        self.conn.text_factory = lambda x: str(x, 'utf-8', 'ignore')
         self.places = place_names
 
 
